04ca45e94f47eba117ebc3c55e339d116b6085a0
Martin Zackrisson <martin.zackrisson@gmail.com>
2012-01-26 23:25:45 +0100

    A pattern matcher and plotter

--- get_all_pattern.py
+++ get_all_pattern.py
@@ -15,14 +15,15 @@ parser = ArgumentParser(description='This script reports all stretches of N long
 parser.add_argument("-i", "--input-file", dest="inputfile", help="Sequence input file.", metavar="PATH")
 parser.add_argument("-f", "--input-format", dest="inputfileformat", help="The format of the sequence file (fasta, fastq)", metavar="FORMAT")
 parser.add_argument("-t", "--threshold", dest="threshold", type=int, default=100,  help="Threshold of minimum length consequtive N:s (default = 100)", metavar="X")
-
+parser.add_argument("-m", "--motif", dest="motif", default="N", type=str, help="Motif to be repeated (default = N)")
 parser.add_argument("-p", "--plot", dest="plot", default=True, type=bool, help="If set causes a plot of contig positions to be drawn.")
-parser.add_argument("-m", "--plot-max-contigs", dest="max_contigs", default=-1, type=int, metavar="X", help="Sets the maximum number of contigs to plot (default unlimited)")
-parser.add_argument("-a", "--hit-alpha", dest="hit_alpha", type=float, help="Set alpha value (0-1) for each marking on cont-plot. Default: 0.1", default=0.1, metavar="0.NN")
+parser.add_argument("-c", "--plot-max-contigs", dest="max_contigs", default=-1, type=int, metavar="X", help="Sets the maximum number of contigs to plot (default unlimited)")
+parser.add_argument("-a", "--hit-alpha", dest="hit_alpha", type=float, help="Set alpha value (0-1) for each marking on cont-plot. Default: 0.9", default=0.9, metavar="0.NN")
 parser.add_argument("-w", "--hit-width", dest="hit_width", type=int, help="Set the width for the hit marking.  Default: 10", default=10, metavar="N")
 
 parser.add_argument("-o", "--output-file", dest="outputfile", help="Path to output file to write data, if unset no output will be written.", metavar="PATH")
 
+parser.add_argument("-v", "--verbose", dest="verbose", help="Makes the script tell you how far it has come (default = 0 (quiet))", default=0, type=int, metavar="N")
 args = parser.parse_args()
 
 known_alt_types = {'fa':'fasta', 'fq':'fastq'}
@@ -39,11 +40,18 @@ if args.inputfileformat == None:
     if args.inputfileformat in known_alt_types.keys():
         args.inputfileformat = known_alt_types[args.inputfileformat]
 
+if args.verbose == None:
+    args.verbose = 0
+
 if args.outputfile != None:
     try:
         fh = open(args.outputfile,'w')
     except:
         parser.error("Unable to create output file")
+
+    fh.write('Id\tStart\tEnd\tSize\n')
+
+ 
 else:
     fh = None
 
@@ -57,15 +65,21 @@ if args.plot == True:
 sequences = SeqIO.parse(args.inputfile,args.inputfileformat)
 
 contig_i = 0
-match_string = 'N{' + str(args.threshold) + ',}'
+match_string = '(' + str(args.motif) + '){' + str(args.threshold) + ',}'
 
-print match_string
+if 4 > args.verbose > 0:
+    print "@Using regular expression: " + match_string
+
+if args.verbose > 1:
+    print 'Id\tStart\tEnd\tSize'
+
+x_labels = []
 
 for rec in sequences:
 
     contig_i += 1
 
-    match_object = re.search(match_string, str(rec.seq))
+    matches = re.finditer(match_string, str(rec.seq))
     #Graphworkds
     if args.plot == True and (args.max_contigs <= contig_i or args.max_contigs==-1):
         X = np.asarray([contig_i*2*args.hit_width, contig_i*2*args.hit_width])
@@ -73,24 +87,39 @@ for rec in sequences:
         #Draw the contig length as it is
         pyplot.plot(X,np.asarray([0, len(rec.seq)]), 'g')
 
-        if match_object != None:
-            if match_object.lastindex != None:
-                for hit in range(match_object.lastindex):
-                    pyplot.plot(X, np.asarray([match_object.start(hit), match_object.stop(hit)]),'r-', alpha=args.alpha)
-            
-                    if fh != None:
+        x_labels.append(rec.id)
 
-                        fh.writeline(req.id + str(match_object.start(hit)) + "\t" + str(match_object.stop(hit)) + "\t" + str(match_object.stop(hit) - match_object.start(hit)))
+        for match in matches:
 
-    elif fh != None and match_object != None:
-        if match_object.lastindex != None:
-            for hit in range(match_object.lastindex):
-        
-                if fh != None:
+            pyplot.plot(X, np.asarray(match.span()),'r-', alpha=args.hit_alpha, linewidth=args.hit_width)
+    
+            if fh != None:
 
-                    fh.writeline(req.id + str(match_object.start(hit)) + "\t" + str(match_object.stop(hit)) + "\t" + str(match_object.stop(hit) - match_object.start(hit)))
+                fh.write(rec.id + "\t" + str(match.start()) + "\t" + \
+                    str(match.end()) + "\t" + \
+                    str(match.end() - match.start()) + '\n')
+
+            if args.verbose > 1:
+                print rec.id + "\t" + str(match.start()) + "\t" + \
+                str(match.end()) + "\t" + \
+                str(match.end() - match.start())
+
+    elif fh != None:
+
+        for match in matches:
+
+            fh.write(rec.id + "\t" + str(match.start()) + "\t" + \
+                str(match.end()) + "\t" + \
+                str(match.end() - match.start()) + '\n')
+
+            if args.verbose > 1:
+                print rec.id + "\t" + str(match.start()) + "\t" + \
+                str(match.end()) + "\t" + \
+                str(match.end() - match.start())
 
 
+    if 3 > args.verbose > 0:
+        print "@Completed: " + str(rec.id)
 
 if fh != None:
     fh.close()
@@ -98,85 +127,8 @@ if fh != None:
 
 if args.plot == True:
 
+    pyplot.xticks(np.arange(1,len(x_labels)+1)*2*args.hit_width,x_labels)
+
     pyplot.show()
 
-'''
-
-
-
-if args.plot == "dist":
-
-    color_pos = 0
-    color_order = "rgbcmyk"
-    if args.markerchar == None:
-        args.markerchar = color_order[color_pos] + '-'
-
-    for pattern in patterns:
-        if pattern in results_dict.keys():
-            X = np.asarray(map(int, results_dict[pattern]))
-            if color_pos > 0:
-                args.markerchar = color_order[color_pos/len(color_order)] + args.markerchar[-1]
-
-
-            if args.low != None:
-                low = int(args.low)
-            else:
-                low = np.min(X)
-
-            if args.high != None:
-                high = int(args.high)
-            else:
-                high = np.max(X)
-
-            X_filter = np.where(np.logical_and(low <= X, X <= high), True, False)
-            X_filtered = X[X_filter]
-            X_sorter = np.argsort(X_filtered)
-            Y = np.asarray(results_dict[patterns[0]].values())
-            Y_filtered = Y[X_filter]
-
-            pyplot.plot(X_filtered[X_sorter], Y_filtered[X_sorter], args.markerchar, markersize=args.markersize)
-        else:
-            print "Pattern", pattern, "is not in parse list"
-
-elif args.plot == "cont":
-    if args.low != None and args.high != None:
-        if len(patterns) > 1:
-            print "This graph only supports one pattern so far (using first)"
-        low = int(args.low)
-        high = int(args.high)
-
-        contig_i = 0
-        x_labels = []
-        for contig in pos_dict[patterns[0]].keys():
-            contig_i += 1
-            x_labels.append(contig)
-            Y =  np.asarray(pos_dict[patterns[0]][contig])
-            Y_diff = Y[1:]-Y[:-1]
-            Y_filtered = np.where(np.logical_and(low <= Y_diff, Y_diff <= high),True, False)
-            Y = Y[Y_filtered]
-
-
-            X = np.ones(Y.shape)* contig_i
-
-            pyplot.plot(np.asarray([contig_i, contig_i]), np.asarray([0,contig_dict[contig]]), 'g-', alpha=1)
-            #if len(X) > 2:
-            #    Y_sizes = np.exp(-(Y[2:] - Y[:-2])/2)*args.markersize
-            #    pyplot.plot(X[1:-1], Y[1:-1], args.markerchar, alpha=args.alpha, markersize=Y_sizes)
-            if 0 < len(X):# < 2:
-                pyplot.plot(X, Y, args.markerchar, alpha=args.alpha, markersize=args.markersize)
-                #pyplot.hexbin(X,Y, gridsize= (1, 1000), bins=1000)
-
-
-        pyplot.xticks(range(1,len(x_labels)+1),x_labels)
-
-    else:
-        
-        parser.print_help()
-        parser.error("This type of plot needs to have -m and -n specified")
-    
-
-pyplot.show()
- 
-
-'''
 
