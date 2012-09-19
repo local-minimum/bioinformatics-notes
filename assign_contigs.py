5ddd3a33727f796681acd6dfd13d22404d7f4b22
Martin Zackrisson <martin.zackrisson@gu.se>
2012-09-19 16:44:06 +0200

    Added binning of contigs and stat module from the same script

---
+++ assign_contigs.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+
+import sys
+from Bio.Blast import NCBIXML
+import numpy as np
+from scipy.stats import ttest_ind
+
+
+def uniq(seq):
+    seen = set()
+    seen_add = seen.add
+    return [ x for x in seq if x not in seen and not seen_add(x)]
+
+
+def get_top_3_evalues(blast_records):
+
+    ret_dict = {}
+
+    for blast_record in blast_records:
+
+        l = []
+        #TOP 3 hits
+        for al in blast_record.alignments[:(len(blast_record.alignments) > 3 
+                and 4 or len(blast_record.alignments))]:
+
+            l.append(al.hsps[0].expect)
+
+        if len(l) > 0:
+            ret_dict[al.title] = l
+
+    return ret_dict
+
+
+def assign_contig(set_A, set_B, alpha=0.01):
+
+    all_keys = uniq(set_A.keys() + set_B.keys())
+
+    for k in all_keys:
+
+        if k in set_A and k in set_B:
+
+            in_set = np.array(set_A[k])
+            out_set =  np.array(set_B[k])
+
+            s = ttest_ind(in_set, out_set)[1]
+
+            if s < alpha and out_set.mean() < in_set.mean():
+
+                del set_A[k]
+
+            else:
+
+                del set_B[k]
+
+    return set_A, set_B
+
+
+def test_dupe_keys(set_A, set_B):
+
+    for k in set_A:
+
+        if k in set_B:
+
+            return True
+
+    return False
+
+
+#SIMPLE HELP
+if len(sys.argv) < 4 or sys.argv[1][:2] == "-h" or sys.argv[1][:3] == "--h":
+
+    print "\nSimple binning algorithm for contigs"
+    print "\n\tUSAGE: {0} [IN GROUP] [OUT GROUP] [FASTA FILE]".format(sys.argv[0])
+    print "\n\nWhere both in and out group should be NCBI compatible"
+    print "BLAST outputs."
+    print "\nThe FASTA-file should be the file that was blasted against both"
+    print "databases."
+
+    sys.exit()
+
+print "\n***STARTING BINNING SCRIPT"
+
+print "\n***CHECKING all input files"
+
+#CHECKING SO FILES EXISTS BEFORE PARSING
+fh = []
+
+for i in xrange(3):
+
+    try:
+        fs = open(sys.argv[1], 'r')
+    except:
+        print "Failed to open BLAST report file '{0}'".format(sys.argv[1+i]),
+        print " for {0} group".format(['in', 'out'][i])
+        sys.exit()
+
+    fh.append(fs)
+
+print "\n***ANALYSING in group results"
+
+#PARSING FILES
+blast_records = NCBIXML.parse(fh[0])
+in_dict = get_top_3_evalues(blast_records)
+fh[0].close()
+
+
+print "\n***ANALYSING out group results"
+
+blast_records = NCBIXML.parse(fh[1])
+out_dict = get_top_3_evalues(blast_records)
+fh[1].close()
+
+print "\n***ASSIGNING contigs to either set"
+
+in_dict, out_dict = assign_contigs(in_dict, out_dict)
+
+print "\n***VERIFYING assignment"
+
+#MAKE SURE THERE ARE NO DUPLICATES
+if test_dupe_keys(in_dict, out_dict):
+
+    print "ERROR: Some keys are in both sets, this should not be"
+    sys.exit()
+
+fs = fh[-1]
+req = list()
+
+in_path = sys.argv[3] + "in_group.fasta"
+out_path = sys.argv[3] + "out_group.fasta"
+
+try:
+
+    write_file = {'in': fs.open(in_path,'w'), 'out': fs.open(out_path, 'w')}
+
+except:
+
+    print "ERROR: Could not open output files!"
+
+print "\n***BINNING contigs into output files"
+
+for line in fs:
+
+    if line[0] == ">":
+
+        if len(req) > 0:
+
+            #Write previous line to where it belongs
+            
+            if req[0] in out_dict:
+
+                write_file['out'].write("\n\r".join(req))
+
+            else:
+
+                write_filt['in'].write("\n\r".join(req))
+
+        req = list()
+
+    req.append(line)
+
+#Closing files
+for f in write_file:
+
+    write_file[f].close()
+
+print "\n***DONE binning contigs"
+print "IN GROUP: {0}".format(in_path)
+print "OUT GROUP: {0}".format(out_path)
---
+++ contig_sort_stat.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+from matplotlib import pyplot as plt
+import numpy as np
+from Bio import SeqIO
+import sys
+
+#CHECK CLI ARGS
+if len(sys.argv) < 3 or sys.argv[1][:2] == "-h" or sys.argv[1][:3] == "--h":
+
+    print "\nBinning stats:"
+    print "\n\tUSAGE: {0} [IN GROUP FAST FILE] [OUT GROUP FASTA FILE]".format(
+                sys.argv[0])
+
+    sys.exit()
+
+print "\n***STARTING stats extraction"
+
+print "\n***PARSING in group file"
+
+in_lens = list()
+
+for seq_record in SeqIO.parse(sys.argv[1], 'fasta'):
+
+    in_lens.append(len(seq_record))
+
+in_lens = sorted(in_lens, reverse=True)
+
+print "\n***PARSING out group file"
+
+out_lens = list()
+
+for seq_record in SeqIO.parse(sys.argv[2], 'fasta'):
+
+    out_lens.append(len(seq_record))
+
+out_lens = sorted(out_lens, reverse=True)
+
+print "\n***Making pie graphs"
+
+graph_path = sys.argv[1] + ".figures."
+
+labels = ['In Group', 'Out Group']
+
+fig = plt.figure()
+ax = fig.gca()
+ax.set_title("Contig Count Distribution")
+sum_in = len(in_lens)
+sum_out = len(out_lens)
+fracs = np.array(sum_in, sum_out) / float(sum_in + sum_out) * 100
+explode = (0.05, 0)
+ax.pie(fracs, explode=explode, labels=labels, autopct="%1.1f%%", shadow=True)
+fig.savefig(graph_path+"count_pie.ps", format='ps')
+
+fig = plt.figure()
+ax = fig.gca()
+ax.set_title("Sequence Length Distribution")
+sum_in = sum(in_lens)
+sum_out = sum(out_lens)
+fracs = np.array(sum_in, sum_out) / float(sum_in + sum_out) * 100
+explode = (0.05, 0)
+ax.pie(fracs, explode=explode, labels=labels, autopct="%1.1f%%", shadow=True)
+fig.savefig(graph_path+"seq_len_pie.ps", format='ps')
+
+print "\n***Calculating accumulative stuff"
+
+joint_lens = sorted(in_lens + out_lens, reversed=True)
+
+for s in (in_len, out_lens, joint_lens):
+
+    for i in xrange(1,len(s)):
+
+        s[i] = s[i] + s[i-1]
+
+
+print "\n***Extening for equal length"
+
+max_l = max(map(len, (in_len, out_lens, joint_lens)))
+
+for s in (in_len, out_lens, joint_lens):
+
+    if len(s) < max_l:
+
+        s += [s[-1]] * (max_l - len(s))
+
+print "\n***Making line graph"
+
+labels.append("Total")
+fig = plt.figure()
+ax = fig.gca()
+ax.set_title("Accumulative Sequence Lengths")
+ax.plot([in_len, out_lens, joint_lens], labels=labels,
+    aa=True)
+ax.set_xticks([0, max_l])
+ax.set_xticklabels([0, 100])
+ax.set_xlabel("Percent of contigs")
+ax.set_ylabel("Accumulative Sequence Length")
+fig.savefig(graph_path+"acc_seq_len.ps", format='ps')
+
